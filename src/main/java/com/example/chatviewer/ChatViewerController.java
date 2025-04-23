ea650e11e0c2b6beba1ab577177a4e19700b0023
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-04-24 00:09:22 +0200

    Comments and stuff for the existing stuff.

--- src/main/java/com/example/chatviewer/ChatViewerController.java
+++ src/main/java/com/example/chatviewer/ChatViewerController.java
@@ -1,28 +1,38 @@
 package com.example.chatviewer;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.text.TextFlow;
 
+/**
+ * Controller class for the Chat Viewer Graphical User Interface (GUI)
+ * Handles button clicks, updates labels and text displays.
+ */
 public class ChatViewerController {
+
+    // Displays the file path and name
     @FXML
     private Label fileLabel;
 
+    // Displays the conversation
     @FXML
     private TextFlow chatTextFlow;
 
+    // Opens dialogue and allows the user to choose and open a file with the conversation
     @FXML
     private Button openMsgFileButton;
 
-
+    /**
+     * Method runs when the file open button is clicked.
+     * Sets
+     */
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onOpenChatButtonClick() {
         fileLabel.setText("Open File Logic.");
     }
 
     public void handleOpenFile() {
-        fileLabel.setText("Open File Logic.");
+        fileLabel.setText("Open File Logic."); // TODO: Still gotta figure this out
     }
 }
