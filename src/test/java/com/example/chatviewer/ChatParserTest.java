99f081210dde5ab930be9915e159ab861a43d544
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-04-25 19:09:54 +0200

    Creation of the unit test for the ChatParser.

---
+++ src/test/java/com/example/chatviewer/ChatParserTest.java
@@ -0,0 +1,11 @@
+package com.example.chatviewer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+
+public class ChatParserTest {
+
+
+}
