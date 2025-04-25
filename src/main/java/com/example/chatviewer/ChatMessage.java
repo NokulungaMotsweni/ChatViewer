e163719e82ddf7b7b02fd3014d070671d3c288fb
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-04-25 14:24:51 +0200

    Add: First version of ChatMessage class.
    Stores individual elements of the chat: timestamp, nickname and content.

--- src/main/java/com/example/chatviewer/ChatMessage.java
+++ src/main/java/com/example/chatviewer/ChatMessage.java
@@ -1,5 +1,27 @@
 package com.example.chatviewer;
 
 public class ChatMessage {
+    // Timestamp of the message (as a raw string from the file)
+    private final String timestamp;
+
+    // Nickname of the user who sent the message
+    private final String nickname;
+
+    // Actual context/text of the message
+    private final String content;
+
+
+    /**
+     * Constructs new ChatMessage object via the parsed message data.
+     *
+     * @param timestamp: Time when message was sent.
+     * @param nickname: Name of sender.
+     * @param content: Actual text of the message.
+     */
+    public ChatMessage(String timestamp, String nickname, String content) {
+        this.timestamp = timestamp;
+        this.nickname = nickname;
+        this.content = content;
+    }
 
 }
