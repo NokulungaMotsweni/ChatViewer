9946f3d5d46cdc0de4900f15f8a00def2d1f2e02
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-04-25 17:56:21 +0200

    Exceptions class that houses all of the exceptions. Currently houses the InvalidMessageFormatException which throws exceptions when the format expected is invalid.

---
+++ src/main/java/com/example/chatviewer/Exceptions.java
@@ -0,0 +1,18 @@
+package com.example.chatviewer;
+
+/**
+ * Contains the customs exceptions for the Chat Viewer application.
+ *All the exceptions are static classes that are all placed here for optimal OOP programming style.
+ */
+public class Exceptions {
+
+    /**
+     * Thrown when the format of the chat in the .msg file is invalid.
+     * Includes the ending of a file unexpectedly.
+     */
+    public static class InvalidMessageFormatException extends Exception {
+        public InvalidMessageFormatException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+}
