aec78149e8e21cb5510183bb980f4b5a3bb0b771
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-17 23:50:36 +0200

    Import Alert to manage alerts.

--- src/main/java/com/example/chatviewer/ChatViewerController.java
+++ src/main/java/com/example/chatviewer/ChatViewerController.java
@@ -1,6 +1,7 @@
 package com.example.chatviewer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.text.TextFlow;
